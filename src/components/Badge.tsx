70cf5c38720a9ffc7506f38c0f7ff59dbd15bc46
yordani <hiyordani@gmail.com>
2024-01-19 14:36:20 -0500

    revised badge value

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Harvest</span>
+        <span> Haven</span>
       </a>
     </span>
   );
